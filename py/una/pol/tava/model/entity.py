b51aa9e5d3690c2649337f7430e2ffdaebf5a808
Abrahan Fretes <abrahan.fretes@gmail.com>
2014-07-01 16:20:13 -0400

    Model - creacion de entidades
    Se crean las entidades de Iteracion, Individuo, Variable y Objetivo, con
    las configuraciones básicas.

--- py/una/pol/tava/model/entity.py
+++ py/una/pol/tava/model/entity.py
@@ -7,6 +7,7 @@ Created on 27/06/2014
 
 
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, SmallInteger
+from sqlalchemy import Float
 from sqlalchemy.orm import relationship
 import base
 
@@ -64,11 +65,6 @@ class Resultado(Base):
     fechaAdd = Column(Date())
     proyecto_id = Column(Integer, ForeignKey('proyecto.id'))
 
-    #==========================================================================
-    # proyecto = relationship('Proyecto',\
-    #                         backref=backref('resultados', order_by=id))
-    #==========================================================================
-
     def __init__(self, nombre, iteracion, etiqueta1, etiqueta2, etiqueta3,\
                  etiqueta4, nombreProblema, numeroObjetivo, numeroVariable,\
                  poblacionInicial, fechaAdd):
@@ -95,6 +91,94 @@ class Resultado(Base):
         self.cantidadIteracion, self.etiqueta1, self.etiqueta2, self.etiqueta3,
         self.etiqueta4, self.nombreProblema, self.numeroObjetivo,
         self.numeroVariable, self.poblacionInicial, self.fechaAdd)
+
+
+class Iteracion(Base):
+    ''''''
+
+    __tablename__ = 'iteracion'
+    id = Column(Integer, primary_key=True)
+    identificador = Column(Integer)
+    inicioEjecucion = Column(Float)
+    finEjecucion = Column(Float)
+    cantidadIndividuo = Column(Integer)
+    resultado_id = Column(Integer, ForeignKey('resultado.id'))
+
+    individuos = relationship('Individuo', order_by='Individuo.id',
+                               backref='iteracion')
+
+    def __init__(self, inicioEjecucion, identificador, finEjecucion,\
+                  cantidadIndividuo):
+        '''
+        Constructor
+        '''
+        self.identificador = identificador
+        self.inicioEjecucion = inicioEjecucion
+        self.finEjecucion = finEjecucion
+        self.cantidadIndividuo = cantidadIndividuo
+
+    def __repr__(self):
+        return "<Iteracion(identificador: '%s', inicioEjecucion:'%s', "\
+            "inicioEjecucion :'%s', finEjecucion:'%s', cantidadindividuo:\
+            '%s')>" % (self.identificador, self.inicioEjecucion,
+                       self.finEjecucion, self.cantidadIndividuo)
+
+
+class Individuo(Base):
+    ''''''
+
+    __tablename__ = 'individuo'
+    id = Column(Integer, primary_key=True)
+    identificador = Column(Integer)
+    varDTLZ = Column(Float)
+    iteracion_id = Column(Integer, ForeignKey('iteracion.id'))
+
+    variables = relationship('Variable', backref='individuo')
+    objetivos = relationship('Objetivo', backref='individuo')
+
+    def __init__(self, identificador, varDTLZ):
+        self.identificador = identificador
+        self.varDTLZ = varDTLZ
+
+    def __repr__(self):
+        return "<Individuo(identificador: '%s', varDTLZ: '%f')>"\
+             % (self.identificador, self.varDTLZ)
+
+
+class Objetivo(Base):
+    ''''''
+
+    __tablename__ = 'objetivo'
+    id = Column(Integer, primary_key=True)
+    orden = Column(Integer)
+    valor = Column(Float)
+    individuo_id = Column(Integer, ForeignKey('individuo.id'))
+
+    def __init__(self, orden, valor):
+        self.orden = orden
+        self.valor = valor
+
+    def __repr__(self):
+        return "<Objetivo(orden: '%s', valor: '%f')>"\
+             % (self.orden, self.valor)
+
+
+class Variable(Base):
+    ''''''
+
+    __tablename__ = 'variable'
+    id = Column(Integer, primary_key=True)
+    orden = Column(Integer)
+    valor = Column(Float)
+    individuo_id = Column(Integer, ForeignKey('individuo.id'))
+
+    def __init__(self, orden, valor):
+        self.orden = orden
+        self.valor = valor
+
+    def __repr__(self):
+        return "<Variable(orden: '%s', valor: '%s')>" \
+            % (self.orden, self.valor)
 
 
 def createDB():
