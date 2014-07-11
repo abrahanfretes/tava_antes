172ee2add882b4b19de1d7a0e0eecdd242d35ef2
Abrahan Fretes <afretes@pol.una.py>
2014-07-11 17:04:03 -0400

    Presenter - proPresenter
    * Se agrega comentario a las funciones de la clase ProyectoPresenter
    y al modulo abm.
    
    refs #sp1 2

--- py/una/pol/tava/model/bd/abm.py
+++ py/una/pol/tava/model/bd/abm.py
@@ -9,16 +9,32 @@ session = base.getSession()
 
 
 def add(objeto):
+    '''
+    Función que persiste un Objeto a la Base de Datos.
+
+    :param objeto: cualquiera de las clases mapeadas a la Base de Datos
+    :return: Objeto
+    '''
     session.add(objeto)
     session.commit()
     return objeto
 
 
 def addAll(listaObjeto):
+    '''
+    Función que persiste una lista de Objetos a la Base de Datos.
+
+    :param listaObjeto: cualquiera de las clases mapeadas a la Base de Datos
+    '''
     session.add_all(listaObjeto)
     session.commit()
 
 
 def delete(objeto):
+    '''
+    Función que remueve uno o varios Objetos de la Base de Datos.
+
+    :param objeto: cualquiera de las clases mapeadas a la Base de Datos
+    '''
     session.delete(objeto)
     session.commit()
--- py/una/pol/tava/presenter/proPresenter.py
+++ py/una/pol/tava/presenter/proPresenter.py
@@ -19,25 +19,56 @@ class ProyectoPresenter():
 
     '''
     def __init__(self):
-        '''
-        Constructor
-        '''
+        pass
 
     def add(self, nombre):
+        '''
+        Función que agrega un Proyecto a la base de datos.
+
+        :param nombre: String, representa el nombre de un proyecto.
+        :return: Proyecto.
+        '''
         proyecto = Proyecto(nombre, None, OPEN, date.today())
         return abm.add(proyecto)
 
     def upDate(self, proyecto):
+        '''
+        Función que actualiza un Proyecto de la base de datos.
+
+        :param proyecto: Proyecto, representa un proyecto en la base de datos.
+        :return: Proyecto.
+        '''
         return abm.add(proyecto)
 
     def delete(self, proyecto):
-        return abm.delete(proyecto)
+        '''
+        Función que elimina un Proyecto de la base de datos.
+
+        :param proyecto: Proyecto, representa un proyecto en la base de datos.
+        '''
+        abm.delete(proyecto)
 
     def getAll(self):
+        '''
+        Función que obtiene todos los Proyectos de la base de datos.
+        :return: Lista de Proyectos.
+        '''
         return query.getAllProject()
 
     def getProjectById(self, idProyecto):
+        '''
+        Función que retorna un Proyecto cuyo id es pasado como parámetro.
+
+        :param idProyecto:
+        :return: Proyecto
+        '''
         return query.getProjectById(idProyecto)
 
     def getProjectByResult(self, resultado):
+        '''
+        Función que retorna un Proyecto tomando como parámetro un Resultado.
+
+        :param resultado:
+        :return: Proyecto
+        '''
         return query.getProjectByResult(resultado)
