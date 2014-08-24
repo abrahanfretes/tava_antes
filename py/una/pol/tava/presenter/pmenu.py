3f804721b89badeda7ff7a4ba1b6ffa1ad3643b2
Arsenio Ferreira <arse.ferreira@gmail.com>
2014-08-24 01:38:13 -0400

    View - menu - reordenamiento
    Se produce un reordenamiento, agrupando cada opcion con su
    correspondiente metodo bind, ademas de proveer comentarios acerca de la
    opcion agrupada.
    
    El metodo UpdateLabels del presenter es movido al view, de manera a que
    el componente pueda hacer tambien uso del seteo de labels.
    
    Producto de la revision de codigo.
    
    refs #16 spr2

--- py/una/pol/tava/presenter/pmenu.py
+++ py/una/pol/tava/presenter/pmenu.py
@@ -5,34 +5,22 @@ Created on 05/08/2014
 '''
 from wx.lib.pubsub import Publisher as pub
 import topic as T
-import py.una.pol.tava.view.vi18n as C
-from wx import GetTranslation as _
 
 
 class MainMenuBarPresenter:
     def __init__(self, iview):
         self.iview = iview
 
-        pub.subscribe(self.OnUpdateLabels, T.LANGUAGE_CHANGED)
+        pub.subscribe(self.UpdateLabelsPub, T.LANGUAGE_CHANGED)
 
-    def OnSelectEnglishLanguage(self):
+    def SelectEnglishLanguage(self):
         pub.sendMessage(T.ENGLISH_SELECTED)
 
-    def OnSelectSpanishLanguage(self):
+    def SelectSpanishLanguage(self):
         pub.sendMessage(T.SPANISH_SELECTED)
 
     def ShowProperties(self):
         pub.sendMessage(T.PROJECT_PROPERTIES)
 
-    def OnUpdateLabels(self, message):
-        self.iview.new_project_menu_item.SetText(_(C.MMB_NP))
-        self.iview.open_menu_item.SetText(_(C.MMB_OP))
-        key_accelerator = '&' + _(C.MMB_PROP) + '\tAlt+Enter'
-        self.iview.properties_item.SetText(key_accelerator)
-        self.iview.exit_menu_item.SetText(_(C.MMB_EXIT))
-        self.iview.english_language_menu_item.SetText(_(C.MMB_EN_US_LA))
-        self.iview.spanish_language_menu_item.SetText(_(C.MMB_ES_PY_LA))
-        self.iview.about_menu_item.SetText(_(C.MMB_ABOUT_TAVA))
-        self.iview.SetMenuLabel(0, _(C.MMB_FILE))
-        self.iview.SetMenuLabel(1, _(C.MMB_LANGUAGE))
-        self.iview.SetMenuLabel(2, _(C.MMB_HELP))
+    def UpdateLabelsPub(self, message):
+        self.iview.SetLabels()
--- py/una/pol/tava/view/vmenu.py
+++ py/una/pol/tava/view/vmenu.py
@@ -27,70 +27,70 @@ class MainMenuBar(wx.MenuBar):
 
         self.presenter = MainMenuBarPresenter(self)
 
-        # Se inicializan los Menus con sus respectivos MenuItems
+        # Se inicializa el menu de Archivo
         file_menu = wx.Menu()
 
-        new_project_menu_item = wx.MenuItem(file_menu, wx.ID_NEW, _(C.MMB_NP))
-        file_menu.AppendItem(new_project_menu_item)
-        self.new_project_menu_item = new_project_menu_item
+        # MenuItem Nuevo Proyecto
+        self.new_project_menu_item = wx.MenuItem(file_menu, wx.ID_NEW)
+        parent.Bind(wx.EVT_MENU, parent.OnProjectNew, id=wx.ID_NEW)
+        file_menu.AppendItem(self.new_project_menu_item)
 
-        open_menu_item = wx.MenuItem(file_menu, wx.ID_OPEN, _(C.MMB_OP))
-        file_menu.AppendItem(open_menu_item)
-        self.open_menu_item = open_menu_item
+        # MenuItem Abrir Proyecto
+        self.open_menu_item = wx.MenuItem(file_menu, wx.ID_OPEN)
+        file_menu.AppendItem(self.open_menu_item)
 
-        key_accelerator = '&' + _(C.MMB_PROP) + '\tAlt+Enter'
-        properties_item = wx.MenuItem(file_menu, wx.ID_PROPERTIES,
-                                      key_accelerator)
-        file_menu.AppendItem(properties_item)
-        self.properties_item = properties_item
+        # MenuItem Propiedades
+        self.properties_item = wx.MenuItem(file_menu, wx.ID_PROPERTIES)
+        file_menu.Bind(wx.EVT_MENU, self.OnPropertiesShow,
+                  self.properties_item)
+        file_menu.AppendItem(self.properties_item)
 
-        exit_menu_item = wx.MenuItem(file_menu, wx.ID_EXIT, _(C.MMB_EXIT),
-                                     '&Quit\tCtrl+Q')
-        file_menu.AppendItem(exit_menu_item)
-        self.exit_menu_item = exit_menu_item
+        # MenuItem Salir de Aplicacion
+        self.exit_menu_item = wx.MenuItem(file_menu, wx.ID_EXIT,
+                                          '&Quit\tCtrl+Q')
+        parent.Bind(wx.EVT_MENU, parent.OnApplicationExit, id=wx.ID_EXIT)
+        file_menu.AppendItem(self.exit_menu_item)
 
+        # Menu de Lenguajes
         language_menu = wx.Menu()
 
-        english_language_menu_item = wx.MenuItem(language_menu, wx.ID_ANY,
-                                                 _(C.MMB_EN_US_LA))
-        language_menu.AppendItem(english_language_menu_item)
-        self.english_language_menu_item = english_language_menu_item
+        # MenuItem del idioma Ingles
+        self.english_language_menu_item = wx.MenuItem(language_menu,
+                                                      wx.ID_ANY, " ")
+        language_menu.Bind(wx.EVT_MENU, self.OnEnglishLanguageSelect,
+                  self.english_language_menu_item)
+        language_menu.AppendItem(self.english_language_menu_item)
 
-        spanish_language_menu_item = wx.MenuItem(language_menu, wx.ID_ANY,
-                                                 _(C.MMB_ES_PY_LA))
-        language_menu.AppendItem(spanish_language_menu_item)
-        self.spanish_language_menu_item = spanish_language_menu_item
+        # MenuItem del idioma Espanhol
+        self.spanish_language_menu_item = wx.MenuItem(language_menu,
+                                                 wx.ID_ANY, " ")
+        language_menu.Bind(wx.EVT_MENU, self.OnSpanishLanguageSelect,
+                  self.spanish_language_menu_item)
+        language_menu.AppendItem(self.spanish_language_menu_item)
 
+        # Menu de Ayuda
         help_menu = wx.Menu()
 
-        about_menu_item = wx.MenuItem(help_menu, wx.ID_ABOUT,
-                                      _(C.MMB_ABOUT_TAVA), wx.EmptyString,
-                                      wx.ITEM_NORMAL)
-        help_menu.AppendItem(about_menu_item)
-        self.about_menu_item = about_menu_item
+        # MenuItem Acerca De
+        self.about_menu_item = wx.MenuItem(help_menu, wx.ID_ABOUT)
+        parent.Bind(wx.EVT_MENU, self.OnAboutBox, id=wx.ID_ABOUT)
+        help_menu.AppendItem(self.about_menu_item)
 
         # Se agrega los Menus al MenuBar Principal
         self.Append(file_menu, _(C.MMB_FILE))
         self.Append(language_menu, _(C.MMB_LANGUAGE))
         self.Append(help_menu, _(C.MMB_HELP))
 
-        parent.Bind(wx.EVT_MENU, parent.OnProjectNew, id=wx.ID_NEW)
-        file_menu.Bind(wx.EVT_MENU, self.OnShowProperties,
-                  self.properties_item)
-        parent.Bind(wx.EVT_MENU, parent.OnApplicationExit, id=wx.ID_EXIT)
-        parent.Bind(wx.EVT_MENU, self.OnAboutBox, id=wx.ID_ABOUT)
-        language_menu.Bind(wx.EVT_MENU, self.OnSelectEnglishLanguage,
-                  english_language_menu_item)
-        language_menu.Bind(wx.EVT_MENU, self.OnSelectSpanishLanguage,
-                  spanish_language_menu_item)
+        # Establecemos los labels de los componentes
+        self.SetLabels()
 
-    def OnSelectEnglishLanguage(self, e):
-        self.presenter.OnSelectEnglishLanguage()
+    def OnEnglishLanguageSelect(self, e):
+        self.presenter.SelectEnglishLanguage()
 
-    def OnSelectSpanishLanguage(self, e):
-        self.presenter.OnSelectSpanishLanguage()
+    def OnSpanishLanguageSelect(self, e):
+        self.presenter.SelectSpanishLanguage()
 
-    def OnShowProperties(self, e):
+    def OnPropertiesShow(self, e):
         self.presenter.ShowProperties()
 
     def OnAboutBox(self, e):
@@ -99,3 +99,21 @@ class MainMenuBar(wx.MenuBar):
         :param e: evento de selección de Menú.
         '''
         AboutDialog()
+
+    def SetLabels(self):
+        # Menu Archivo y sus items
+        self.SetMenuLabel(0, _(C.MMB_FILE))
+        self.new_project_menu_item.SetText(_(C.MMB_NP))
+        self.open_menu_item.SetText(_(C.MMB_OP))
+        key_accelerator = '&' + _(C.MMB_PROP) + '\tAlt+Enter'
+        self.properties_item.SetText(key_accelerator)
+        self.exit_menu_item.SetText(_(C.MMB_EXIT))
+
+        # Menu Idioma y sus items
+        self.SetMenuLabel(1, _(C.MMB_LANGUAGE))
+        self.english_language_menu_item.SetText(_(C.MMB_EN_US_LA))
+        self.spanish_language_menu_item.SetText(_(C.MMB_ES_PY_LA))
+
+        # Menu Ayuda y sus items
+        self.SetMenuLabel(2, _(C.MMB_HELP))
+        self.about_menu_item.SetText(_(C.MMB_ABOUT_TAVA))
