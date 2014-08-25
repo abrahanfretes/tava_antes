0c1c953c7091c90831978919ae80d976f86c8869
Arsenio Ferreira <aferreira@pol.una.py>
2014-08-25 17:47:02 -0400

    View - vbody actualizacion
    Reordenamiento de codigo del modulo, ademas de agregar descripciones
    a los pasos seguidos en la definicion de las clases y la configuracion
    de sus atributos.
    
    Producto de la revision de codigo.
    
    refs #16 spr2

--- py/una/pol/tava/view/vbody.py
+++ py/una/pol/tava/view/vbody.py
@@ -6,7 +6,6 @@ Created on 28/05/2014
 '''
 import wx
 
-from py.una.pol.tava.model.mproject import ProjectModel
 from py.una.pol.tava.view.vtree import ProjectTreeCtrl
 from py.una.pol.tava.presenter.pbody import ProjectTreeNotebookPresenter
 from wx import GetTranslation as _
@@ -14,41 +13,10 @@ import py.una.pol.tava.view.vi18n as C
 import py.una.pol.tava.view.vimages as I
 
 
-class TreePanel(wx.Panel):
-    def __init__(self, parent, main_frame):
-        super(TreePanel, self).__init__(parent)
-
-        # Creamos el arbol de Proyectos
-        self.project_tree = ProjectTreeCtrl(self)
-
-        # Creamos un boxSizer para el arbol de Proyectos
-        project_tree_vsizer = wx.BoxSizer(wx.VERTICAL)
-        project_tree_vsizer.Add(self.project_tree, 1, wx.EXPAND | wx.ALL, 3)
-        self.SetSizer(project_tree_vsizer)
-
-
-class ProjectTreeNotebook(wx.Notebook):
-    def __init__(self, parent, main_frame):
-        super(ProjectTreeNotebook, self).__init__(parent, style=wx.BK_DEFAULT)
-
-        self.presenter = ProjectTreeNotebookPresenter(self)
-
-        self.project_tree_panel = TreePanel(self, main_frame)
-        self.AddPage(self.project_tree_panel, _(C.MP_PE))
-
-        # Se configura la pestaña de navegación de proyectos.
-        il = wx.ImageList(16, 16)
-        self.SetImageList(il)
-        tree_explorer_bmp = il.Add(I.tree_explorer_gif)
-        self.SetPageImage(0, tree_explorer_bmp)
-        self.SetBackgroundColour(self.GetThemeBackgroundColour())
-
-
 class MainPanel(wx.Panel):
     '''
-    Clase que representa al Cuerpo Principal que contendrá el panel de
-    navegación de Proyectos, área de trabajo y panel de propiedades de los
-    objetos.
+    Clase Panel que representa al Cuerpo Principal que contendrá el panel de
+    navegación de Proyectos, área de trabajo, etc.
     '''
 
     def __init__(self, parent):
@@ -57,50 +25,104 @@ class MainPanel(wx.Panel):
         :param parent: referencia a la clase padre de MainPanel.
         '''
         super(MainPanel, self).__init__(parent)
+
+        # Seteamos el color de fondo
         self.SetBackgroundColour('#3B444B')
 
+        # Definicion del Splitter que divide el area de trabajo de la
+        # exploración de proyectos
         self.splitter = wx.SplitterWindow(self, style=wx.SP_3D)
 
-        # Se define un panel izquierdo que contendra el espacio de navegación
-        # de los proyectos y una Notebook para la pestaña de navegación.
+        # Definicion del panel izquierdo que contendra el panel de explorcion
+        # de proyectos.
         left_panel = wx.Panel(self.splitter)
         left_panel.SetBackgroundColour('#FFFFFF')
-        self.project_tree_notebook = ProjectTreeNotebook(left_panel, parent)
+
+        # Definicion de la clase Notebook para el explorador de proyectos
+        self.project_tree_notebook = ProjectTreeNotebook(left_panel)
+
+        # Definicion del Sizer para la clase Notebook
         project_tree_sizer = wx.BoxSizer()
         project_tree_sizer.Add(self.project_tree_notebook, 1, wx.EXPAND)
 
+        # Asociamos el sizer al panel izquierdo
         left_panel.SetSizer(project_tree_sizer)
 
-        # Creamos el panel derecho para el splitter
+        # Definicion del panel derecho para el splitter
         right_panel = wx.Panel(self.splitter, -1)
 
+        # Definicion del componente Notebook para el panel derecho
         right_notebook = wx.Notebook(right_panel, style=wx.SP_BORDER)
-        dpanel = wx.Panel(right_notebook, -1)
-        right_notebook.AddPage(dpanel, "Tab 1")
 
-        # lado derecho del área de trabajo
+        # Definicion del panel que estara contenido en la notebook
+        dpanel = wx.Panel(right_notebook, -1)
+
+        # Establecemos el panel creado como pestaña para la notebook creada
+        right_notebook.AddPage(dpanel, "Tab 1")
 
         # Creamos el sizer para el contenido del panel derecho
         right_panel_hsizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Establecemos la notebook creada en el sizer
         right_panel_hsizer.Add(right_notebook, 1, wx.EXPAND)
+
+        # Asociamos el sizer con el panel derecho
         right_panel.SetSizer(right_panel_hsizer)
 
+        # Asociamos los paneles izquierdo y derecho al splitter
         self.splitter.SplitVertically(left_panel, right_panel, 300)
         self.splitter.SetMinimumPaneSize(200)
 
-        # Creamos el sizer para colocar el widget Splitter y expandirlo
-        # en el Cuerpo Principal
+        # Creamos el sizer para colocar el Splitter y expandirlo
+        # en el MainPanel
         body_sizer = wx.BoxSizer(wx.VERTICAL)
         body_sizer.Add(self.splitter, 1, wx.EXPAND | wx.ALL, 10)
+
+        # Asociamos el sizer creado con la clase MainPanel
         self.SetSizer(body_sizer)
 
-    def LoadProjects(self):
-        proPresenter = ProjectModel()
-        namesProjects = []
-        for p in proPresenter.getAll():
-            self.project_tree.AddProjectNode(self.project_tree.root,
-                                              p.nombre, p.id)
-            namesProjects.append(p.name)
-        if self.project_tree.root:
-            self.project_tree.SortChildren(self.project_tree.root)
-        return namesProjects
+
+class ProjectTreeNotebook(wx.Notebook):
+    '''
+    Clase Notebook que contendrá la pestaña que albergará el árbol de
+    proyectos.
+    '''
+    def __init__(self, parent):
+        super(ProjectTreeNotebook, self).__init__(parent, style=wx.BK_DEFAULT)
+
+        # Definición del Presenter
+        self.presenter = ProjectTreeNotebookPresenter(self)
+
+        # Creacion del panel contenedor del arbol de Proyectos
+        self.project_tree_panel = TreePanel(self)
+
+        # El panel es agregado como pagina del componente Notebook con su
+        # correspondiente label
+        self.AddPage(self.project_tree_panel, _(C.MP_PE))
+
+        # Se añade el icono de explorador de proyectos a la unica pestaña.
+        il = wx.ImageList(16, 16)
+        self.SetImageList(il)
+        tree_explorer_bmp = il.Add(I.tree_explorer_gif)
+        self.SetPageImage(0, tree_explorer_bmp)
+
+        # Se establece el color de fondo para el componente Notebook.
+        self.SetBackgroundColour(self.GetThemeBackgroundColour())
+
+
+class TreePanel(wx.Panel):
+    '''
+    Clase Panel que contendrá el árbol de exploración de proyectos.
+    '''
+    def __init__(self, parent):
+        super(TreePanel, self).__init__(parent)
+
+        # Creamos el arbol de Proyectos y lo asociamos al Panel
+        self.project_tree = ProjectTreeCtrl(self)
+
+        # Creamos un boxSizer para el arbol de Proyectos y lo añadimos al Panel
+        project_tree_vsizer = wx.BoxSizer(wx.VERTICAL)
+        project_tree_vsizer.Add(self.project_tree, 1, wx.EXPAND | wx.ALL, 3)
+
+        # Asociamos el sizer a la clase Panel
+        self.SetSizer(project_tree_vsizer)
