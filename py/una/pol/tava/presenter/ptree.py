69bf8a7e396af66b097917adac7e5142f4335d63
Arsenio Ferreira <aferreira@pol.una.py>
2014-08-28 17:49:31 -0400

    View - ptree actualizacion
    Aplicacion de la convencion de nombres a los metodos, ademas de
    reorganizar el codigo en la clase presenter.
    
    Resultado de la verificacion de codigo.
    
    refs #16 spr2

--- py/una/pol/tava/presenter/ptree.py
+++ py/una/pol/tava/presenter/ptree.py
@@ -6,99 +6,91 @@ Created on 28/07/2014
 from wx.lib.pubsub import Publisher as pub
 from py.una.pol.tava.model.mproject import ProjectModel
 from py.una.pol.tava.base.entity import OPEN, CLOSED, HIDDEN
-import topic as t
+import topic as T
 
 
 class ProjectTreeCtrlPresenter:
     def __init__(self, iview):
         self.iview = iview
 
-        pub.subscribe(self.OnNewPub, t.PROJECT_NEW)
+        pub.subscribe(self.NewProjectPub, T.PROJECT_NEW)
+        pub.subscribe(self.DeleteSelectProjectPub, T.PROJECT_DELETE_CLICK)
+        pub.subscribe(self.DeleteProjectPub, T.PROJECT_DELETE_OK)
+        pub.subscribe(self.CloseProjectPub, T.PROJECT_CLOSE)
+        pub.subscribe(self.OpenProjectPub, T.PROJECT_OPEN)
+        pub.subscribe(self.RenameProjectPub, T.PROJECT_RENAME_UP)
+        pub.subscribe(self.HideProjectPub, 'PROJECT.HIDE')
+        pub.subscribe(self.UnHideProjectPub, 'PROJECT.LISTRESTORE')
 
-        pub.subscribe(self.OnDeleteClickPub, t.PROJECT_DELETE_CLICK)
-        pub.subscribe(self.OnDeletedOkPub, t.PROJECT_DELETE_OK)
+    def NewProjectPub(self, message):
+        project = message.data
+        self.AddNode(project)
 
-        pub.subscribe(self.OnClosedPub, t.PROJECT_CLOSE)
-        pub.subscribe(self.OnOpenPub, t.PROJECT_OPEN)
-        pub.subscribe(self.OnRenameUpPub, t.PROJECT_RENAME_UP)
-        pub.subscribe(self.OnHidePub, 'PROJECT.HIDE')
-        pub.subscribe(self.OnUnHidePub, 'PROJECT.LISTRESTORE')
+    def DeleteSelectProjectPub(self, message):
+        pub.sendMessage(T.PROJECT_DELETE_SELECT)
 
-    def OnUnHidePub(self, message):
+    def DeleteProjectPub(self, message):
+        project = self.GetProjectSelected()
+        ProjectModel().delete(project)
+        self.DeleteProjectItem()
+
+    def CloseProjectPub(self, message):
+        project = self.GetProjectSelected()
+        project.state = CLOSED
+        project = ProjectModel().upDate(project)
+        self.UpdateProjectTree(project)
+
+    def OpenProjectPub(self, message):
+        project = self.GetProjectSelected()
+        project.state = OPEN
+        project = ProjectModel().upDate(project)
+        self.UpdateProjectTree(project)
+
+    def RenameProjectPub(self, message):
+        project = message.data
+        self.UpdateProjectTree(project)
+
+    def HideProjectPub(self, message):
+        project = self.GetProjectSelected()
+        project.state = HIDDEN
+        project = ProjectModel().upDate(project)
+        self.DeleteProjectItem()
+
+    def UnHideProjectPub(self, message):
         list_names = message.data
         for name in list_names:
             project = self.UpDateOpenForHideProject(name)
-            self.OnAddNode(project)
+            self.AddNode(project)
+
+    def UpdateProjectTree(self, project):
+        self.DeleteProjectItem()
+        self.AddNode(project)
 
     def UpDateOpenForHideProject(self, name_project):
         project = ProjectModel().getProjectForName(name_project)
         project.state = OPEN
         return ProjectModel().upDate(project)
 
-    def OnAddNode(self, project):
+    def AddNode(self, project):
         self.iview.AddProjectNode(project)
 
-    def OnDelete(self):
-        self.iview.OnDeleteItem(self.iview.GetSelection())
+    def DeleteProjectItem(self):
+        self.iview.DeleteProjectItem(self.iview.GetSelection())
 
-    def OnInitializeTree(self):
+    def InitializeTree(self):
         list_project = ProjectModel().getAll()
-        self.iview.OnInitializeTree(list_project)
+        self.iview.LoadProjectsInTree(list_project)
 
-    def OnSelectedProjectSend(self):
-        pub.sendMessage(t.PROJECT_SELECTED, self.GetProjectSelected())
+    def SendSelectedProject(self):
+        pub.sendMessage(T.PROJECT_SELECTED, self.GetProjectSelected())
         if self.GetProjectSelected().state == OPEN:
-            pub.sendMessage(t.PROJECT_SELECTED_OPEN)
+            pub.sendMessage(T.PROJECT_SELECTED_OPEN)
         else:
-            pub.sendMessage(t.PROJECT_SELECTED_CLOSE)
+            pub.sendMessage(T.PROJECT_SELECTED_CLOSE)
 
     def GetNamesProjects(self):
         return ProjectModel().getNamesProject()
 
-    def OnUpDateTree(self, project):
-        self.OnDelete()
-        self.OnAddNode(project)
-
-    def OnClose(self):
-        project = self.GetProjectSelected()
-        project.state = CLOSED
-        project = ProjectModel().upDate(project)
-        self.OnUpDateTree(project)
-
-    def OnOpen(self):
-        project = self.GetProjectSelected()
-        project.state = OPEN
-        project = ProjectModel().upDate(project)
-        self.OnUpDateTree(project)
-
-    def OnRenameUpPub(self, message):
-        project = message.data
-        self.OnUpDateTree(project)
-
-    def OnNewPub(self, message):
-        project = message.data
-        self.OnAddNode(project)
-
-    def OnDeleteClickPub(self, message):
-        pub.sendMessage(t.PROJECT_DELETE_SELECT, self.GetProjectSelected())
-
-    def OnDeletedOkPub(self, message):
-        project = self.GetProjectSelected()
-        ProjectModel().delete(project)
-        self.OnDelete()
-
-    def OnClosedPub(self, message):
-        self.OnClose()
-
-    def OnOpenPub(self, message):
-        self.OnOpen()
-
     def GetProjectSelected(self):
         item_selected = self.iview.GetSelection()
         return self.iview.GetItemPyData(item_selected)
-
-    def OnHidePub(self, message):
-        project = self.GetProjectSelected()
-        project.state = HIDDEN
-        project = ProjectModel().upDate(project)
-        self.OnDelete()
