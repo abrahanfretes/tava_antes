8c53804b5c47f9709cc0c748c14567e7739672e8
Abrahan Fretes <abrahan.fretes@gmail.com>
2014-09-24 17:20:21 -0400

    Configuración de Tava
    * Se cmabia la función createDB() que esllamada desde tava.py, antes
    hacía referencia al módulo base pero desde ahora hará referencia al que
    esta en entity.
    
    refs #5 spr4

--- py/una/pol/tava/tava.py
+++ py/una/pol/tava/tava.py
@@ -5,14 +5,14 @@ Created on 27/06/2014
 @author: aferreira
 '''
 import wx
-from py.una.pol.tava.base import base
+from py.una.pol.tava.base.entity import createDB
 
 
 def main():
     '''
     Método principal de ejecución del programa---
     '''
-    base.createDb()
+    createDB()
     ex = wx.App()
     from py.una.pol.tava.view.vframe import MainFrame
     frame = MainFrame(None)
