8a73a5d24ff24e82346c045120b2039a7880098e
Abrahan Fretes <abrahan.fretes@gmail.com>
2015-03-24 19:13:43 -0400

    Funcionalidad Grafico Coordenadas Paralelas
    Se agregan nuevos modulos encargados de los graficos de coordenadas
    paralelas, este módulo permite solo ver las figuras.
    
    Signed-off-by: Abrahan Fretes <abrahan.fretes@gmail.com>

---
+++ py/una/pol/tava/presenter/pparallelcoordinates2.py
@@ -0,0 +1,245 @@
+'''
+Created on 17/3/2015
+
+@author: abrahan
+'''
+import wx
+from wx.lib.pubsub import Publisher as pub
+import topic as T
+
+from py.una.pol.tava.model.mresult import ResultModel as rm
+from py.una.pol.tava.model.miteration import InterationModel as im
+from py.una.pol.tava.model.mindividual import IndividualModel as inm
+
+TYPE_TEST = 0
+TYPE_RESULT = 1
+TYPE_ITERATION = 2
+
+
+class WorkingPageParallelGFPresenter:
+    def __init__(self, iview):
+        self.iview = iview
+
+        pub.subscribe(self.prepareFileForTestFigurePub,
+                      T.PARALLEL_FIGURE_FILE_TEST_GRAFIC_GF)
+
+        pub.subscribe(self.prepareFileForResultFigurePub,
+                      T.PARALLEL_FIGURE_FILE_RESU_GRAFIC_GF)
+
+        pub.subscribe(self.prepareFileForIterationFigurePub,
+                      T.PARALLEL_FIGURE_FILE_ITER_GRAFIC_GF)
+
+    def prepareFileForTestFigurePub(self, message):
+        self.createFileForTest(message.data)
+        pub.sendMessage(T.PARALLEL_FIGURE_VIEW_TEST_GRAFIC_GF, message.data)
+
+    def prepareFileForResultFigurePub(self, message):
+        self.createFileForResult(message.data)
+        pub.sendMessage(T.PARALLEL_FIGURE_VIEW_RESULT_GRAFIC_GF, message.data)
+
+    def prepareFileForIterationFigurePub(self, message):
+        self.createFileForIteration(message.data)
+        pub.sendMessage(T.PARALLEL_FIGURE_VIEW_ITERA_GRAFIC_GF, message.data)
+
+    def createFileForTest(self, ite_list):
+        inm().createFileTestGF(ite_list)
+
+    def createFileForResult(self, ite_list):
+        inm().createFileResultGF(ite_list)
+
+    def createFileForIteration(self, ite_list):
+        inm().createFileIterationtGF(ite_list)
+
+
+class ParallelTreeGFPresenter:
+    def __init__(self, iview, test):
+
+        self.iview = iview
+        self.test = test
+        self.root = self.iview.AddRoot("Test Data")
+        self.checkeds_last = []
+        self.less_type_gf = None
+
+        pub.subscribe(self.prepareTestFigurePub,
+                      T.PARALLEL_FIGURE_BUTTON_TEST_GRAFIC_GF)
+        pub.subscribe(self.prepareResultFigurePub,
+                      T.PARALLEL_FIGURE_BUTTON_RESU_GRAFIC_GF)
+        pub.subscribe(self.prepareIteraFigurePub,
+                      T.PARALLEL_FIGURE_BUTTON_ITER_GRAFIC_GF)
+
+        # inicializamos el arbol
+        for detail in test.test_details:
+            r_name = rm().getNameById(detail.result_id)
+            td_item = self.iview.AppendItem(self.root, r_name)
+            self.iview.SetItemPyData(td_item, '')
+            self.iview.SetItemImage(td_item, 0, wx.TreeItemIcon_Normal)
+
+            for data in detail.test_datas:
+                idn = str(im().getIdentifierById(data.iteration_id))
+                tda_item = self.iview.AppendItem(td_item, idn, ct_type=1)
+                self.iview.SetItemPyData(tda_item, data.iteration_id)
+                self.iview.CheckItem(tda_item, False)
+
+        # ordenamos el arbol
+        self.iview.SortChildren(self.root)
+
+        # expandimos el arbol
+        for item in self.root.GetChildren():
+            self.iview.Expand(item)
+
+        # inicializamos la lista de chequeados
+        self.checkeds_last = sorted(self.getListChecked())
+
+    def isChangeChecked(self):
+        aux = sorted(self.getListChecked())
+        if self.checkeds_last != aux:
+            self.checkeds_last = aux
+            return True
+        return False
+
+    def getListChecked(self, true=True):
+        to_dicc = []
+        for item_result in self.root.GetChildren():
+            for item_ite in item_result.GetChildren():
+                if true == self.iview.IsItemChecked(item_ite):
+                    to_dicc.append(self.iview.GetItemPyData(item_ite))
+        return to_dicc
+
+    def getListCheckedForResult(self, true=True):
+        r_list = []
+        for item_result in self.root.GetChildren():
+            i_list = []
+            for item_ite in item_result.GetChildren():
+                if true == self.iview.IsItemChecked(item_ite):
+                    i_list.append(self.iview.GetItemPyData(item_ite))
+            r_list.append(i_list)
+        return r_list
+
+    def getLenListChecked(self):
+        return len(self.getListChecked())
+
+    def getCorrectCountChecked(self):
+        list_checked = self.getLenListChecked()
+        return (list_checked > 0 and list_checked < 5)
+
+    def setCheckedGF(self):
+        pub.sendMessage(T.PARALLEL_FIGURE_TREE_CHECK_GF,
+                        self.getCorrectCountChecked())
+
+    def prepareTestFigurePub(self, message):
+        if self.canSendMessage(message.data):
+            pub.sendMessage(T.PARALLEL_FIGURE_FILE_TEST_GRAFIC_GF,
+                            tuple(self.getListChecked()))
+
+    def prepareResultFigurePub(self, message):
+        if self.canSendMessage(message.data):
+            pub.sendMessage(T.PARALLEL_FIGURE_FILE_RESU_GRAFIC_GF,
+                            tuple(self.getListCheckedForResult()))
+
+    def prepareIteraFigurePub(self, message):
+        if self.canSendMessage(message.data):
+            pub.sendMessage(T.PARALLEL_FIGURE_FILE_ITER_GRAFIC_GF,
+                            tuple(self.getListChecked()))
+            pass
+
+    def canSendMessage(self, type_gf):
+        if(self.less_type_gf != type_gf):
+            self.less_type_gf = type_gf
+            return True
+        return self.isChangeChecked()
+
+from pandas.tools.plotting import parallel_coordinates
+
+
+class ParallelFigureGFPresenter:
+    def __init__(self, iview, test):
+        self.iview = iview
+
+        self.test = test
+
+        pub.subscribe(self.prepareViewTestFigurePub,
+                      T.PARALLEL_FIGURE_VIEW_TEST_GRAFIC_GF)
+
+        pub.subscribe(self.prepareViewResultFigurePub,
+                      T.PARALLEL_FIGURE_VIEW_RESULT_GRAFIC_GF)
+
+        pub.subscribe(self.prepareViewIterationFigurePub,
+                      T.PARALLEL_FIGURE_VIEW_ITERA_GRAFIC_GF)
+
+    def prepareViewTestFigurePub(self, message):
+        self.cleanParallelFigure()
+        axe = self.iview.figure.gca()
+        df = inm().getCsvForTest()
+        axe = parallel_coordinates(df, 'Name', ax=axe)
+        self.iview.canvas.draw()
+
+    def prepareViewResultFigurePub(self, message):
+
+        ite_list = message.data
+        self.cleanParallelFigure()
+
+        count_r = len(ite_list)
+        axe_plot = 100 * count_r + 11
+
+        for i_list in ite_list:
+            axe = self.iview.figure.add_subplot(axe_plot)
+            df = inm().getCsvForResult(i_list)
+            axe = parallel_coordinates(df, 'Name', ax=axe)
+            self.iview.canvas.draw()
+            axe_plot += 1
+
+    def prepareViewIterationFigurePub(self, message):
+
+        ite_list = message.data
+        self.cleanParallelFigure()
+
+        count_r = len(ite_list)
+        axe_plot = 100 * count_r + 11
+
+        for iteration in ite_list:
+            axe = self.iview.figure.add_subplot(axe_plot)
+            df = inm().getCsvForIteration(iteration)
+            axe = parallel_coordinates(df, 'Name', ax=axe)
+            self.iview.canvas.draw()
+            axe_plot += 1
+
+    def cleanParallelFigure(self):
+        for axe in self.iview.figure.get_axes():
+            self.iview.figure.delaxes(axe)
+
+
+class ButtonsTollFigureGFPresenter:
+    def __init__(self, iview):
+        self.iview = iview
+
+        pub.subscribe(self.checkedTreePub, T.PARALLEL_FIGURE_TREE_CHECK_GF)
+        self.setEnable(False)
+
+    def checkedTreePub(self, message):
+        self.setEnable(message.data)
+
+    def setEnable(self, enable):
+        if enable:
+            self.enableButtons()
+        else:
+            self.disableButtons()
+
+    def enableButtons(self):
+        self.iview.test.Enable()
+        self.iview.result.Enable()
+        self.iview.iteration.Enable()
+
+    def disableButtons(self):
+        self.iview.test.Disable()
+        self.iview.result.Disable()
+        self.iview.iteration.Disable()
+
+    def testGrafic(self):
+        pub.sendMessage(T.PARALLEL_FIGURE_BUTTON_TEST_GRAFIC_GF, TYPE_TEST)
+
+    def resultGrafic(self):
+        pub.sendMessage(T.PARALLEL_FIGURE_BUTTON_RESU_GRAFIC_GF, TYPE_RESULT)
+
+    def iterationGrafic(self):
+        pub.sendMessage(T.PARALLEL_FIGURE_BUTTON_ITER_GRAFIC_GF,
+                        TYPE_ITERATION)
---
+++ py/una/pol/tava/view/vparallelcoordinates2.py
@@ -0,0 +1,144 @@
+'''
+Created on 17/3/2015
+
+@author: abrahan
+'''
+import wx
+import py.una.pol.tava.view.vimages as I
+
+
+# -------------------         Panel Principal          ------------------------
+# -------------------                                  ------------------------
+from py.una.pol.tava.presenter.pparallelcoordinates2\
+    import WorkingPageParallelGFPresenter
+
+
+class WorkingPageParallelGF(wx.Panel):
+    def __init__(self, parent, test):
+        wx.Panel.__init__(self, parent)
+
+        #  ------ self customize -----------------------------------------
+        #  ------ self components ----------------------------------------
+        self.presenter = WorkingPageParallelGFPresenter(self)
+        tree = ParallelTreeGF(self, test)
+        figure = ParallelFigureGF(self, test)
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(tree, 1, wx.EXPAND)
+        sizer.Add(figure, 3, wx.EXPAND)
+        self.SetSizer(sizer)
+        #  ------ self controls ------------------------------------------
+
+# -------------------         Panel Tree               ------------------------
+# -------------------                                  ------------------------
+import wx.lib.agw.customtreectrl as CT
+from py.una.pol.tava.presenter.pparallelcoordinates2\
+    import ParallelTreeGFPresenter
+
+
+class ParallelTreeGF(CT.CustomTreeCtrl):
+    def __init__(self, parent, test):
+        CT.CustomTreeCtrl.__init__(self, parent, agwStyle=CT.TR_HIDE_ROOT)
+
+        #  ------ self customize -----------------------------------------
+        il = wx.ImageList(16, 16)
+        self.file_bmp = il.Add(I.filegraph_png)
+        self.AssignImageList(il)
+        self.SetBackgroundColour('#D9F0F8')
+        #  ------ self components ----------------------------------------
+        self.presenter = ParallelTreeGFPresenter(self, test)
+
+        self.Bind(CT.EVT_TREE_ITEM_CHECKED, self.OnChecked)
+        #  ------ self controls ------------------------------------------
+
+    def OnChecked(self, event):
+        self.presenter.setCheckedGF()
+
+
+# -------------------         Panel Figure             ------------------------
+# -------------------                                  ------------------------
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_wxagg import NavigationToolbar2Wx as Toolbar
+
+from py.una.pol.tava.presenter.pparallelcoordinates2\
+    import ParallelFigureGFPresenter
+
+
+class ParallelFigureGF(wx.Panel):
+    '''
+    '''
+    def __init__(self, parent, test):
+        wx.Panel.__init__(self, parent)
+
+        #  ------ self customize -----------------------------------------
+        #  ------ self components ----------------------------------------
+        self.parent = parent
+
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.toolbar = Toolbar(self.canvas)
+        self.toolbar.Realize()
+
+        self.button_tolbar = ButtonsTollFigureGF(self)
+
+        self.sizer_h = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer_h.Add(self.button_tolbar, 4, wx.EXPAND)
+        self.sizer_h.Add(self.toolbar, 1)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.sizer_h, 0, wx.LEFT | wx.EXPAND)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+        self.presenter = ParallelFigureGFPresenter(self, test)
+        #  ------ self controls ------------------------------------------
+
+# -------------------         Panel Toll Figure        ------------------------
+# -------------------                                  ------------------------
+from py.una.pol.tava.presenter.pparallelcoordinates2\
+    import ButtonsTollFigureGFPresenter
+
+
+class ButtonsTollFigureGF(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+
+        # ------ self customize ---------------------------------------
+        self.SetBackgroundColour('#f8f1d9')
+
+        # ------ self components --------------------------------------
+
+        self.test = wx.Button(self, -1, 'For Test')
+        self.test.SetToolTipString("Un grafico para el Test")
+
+        self.result = wx.Button(self, -1, 'For Result')
+        self.result.SetToolTipString("Un grafico por Resultado")
+
+        self.iteration = wx.Button(self, -1, 'For Iteration')
+        self.iteration.SetToolTipString("Un grafico por Iteracion")
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.test, 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT, border=5)
+        sizer.Add(self.result, 0, wx.ALIGN_CENTER_VERTICAL)
+        sizer.Add(self.iteration, 0, wx.ALIGN_CENTER_VERTICAL)
+        self.SetSizer(sizer)
+
+        self.presenter = ButtonsTollFigureGFPresenter(self)
+
+        # ------ self inicailes executions ----------------------------
+        self.Bind(wx.EVT_BUTTON, self.OnTestGrafic, self.test)
+        self.Bind(wx.EVT_BUTTON, self.OnResultGrafic, self.result)
+        self.Bind(wx.EVT_BUTTON, self.OnIterationGrafic, self.iteration)
+
+    # ------ self controls --------------------------------------------
+
+    def OnTestGrafic(self, event):
+        self.presenter.testGrafic()
+
+    def OnResultGrafic(self, event):
+        self.presenter.resultGrafic()
+
+    def OnIterationGrafic(self, event):
+        self.presenter.iterationGrafic()
