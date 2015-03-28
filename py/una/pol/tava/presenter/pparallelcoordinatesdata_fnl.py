528a3d35c5064f67bba8300056690fe41de855a7
Abrahan Fretes <abrahan.fretes@gmail.com>
2015-03-28 10:46:50 -0400

    Eliminación de Archivos
    Se eliminan los archivos que no se usaran mas, estos son, los módulos
    de graficos de coordenadas paralelas iniciales.
    
    Signed-off-by: Abrahan Fretes <abrahan.fretes@gmail.com>

--- py/una/pol/tava/presenter/pparallelcoordinatesdata_fnl.py
+++ py/una/pol/tava/presenter/pparallelcoordinatesdata_fnl.py
@@ -12,9 +12,6 @@ from py.una.pol.tava.model.miteration import InterationModel as im
 from py.una.pol.tava.model.mindividual import IndividualModel as inm
 from py.una.pol.tava.model.mparallel_analizer import ParallelAnalizerModel
 
-from py.una.pol.tava.presenter.pparallelcoordinates import\
-    parallel_coordinatesTava
-
 
 class TopPanelPresenter:
     def __init__(self, iview, test, mode):
@@ -523,3 +520,116 @@ class AddFilterObjetivesScrollPresenter:
                     max_list.append(value.getMaxValue())
                     break
         return max_list, min_list
+
+
+def parallel_coordinatesTava(frame, class_column, len_color=1, pos_color=0,
+                             ax=None, no_seque=True, is_use_legends=True,
+                             cols=None, color=None, use_columns=False,
+                             xticks=None, colormap=None, **kwds):
+    """Parallel coordinates plotting.
+
+    Parameters
+    ----------
+    frame: DataFrame
+    class_column: str
+        Column name containing class names
+    len_color: int,, default 1
+        Color amount taken into account
+    pos_color: int, default 0
+        Position of the color to be used
+    ax: matplotlib.axis, optional
+        matplotlib axis object
+    no_seque: boolean, default True
+        Indicates type of graph
+    is_use_legends: boolean, default True
+        To use for show legend.
+    cols: list, optional
+        A list of column names to use
+    color: list or tuple, optional
+        Colors to use for the different classes
+    use_columns: bool, optional
+        If true, columns will be used as xticks
+    xticks: list or tuple, optional
+        A list of values to use for xticks
+    colormap: str or matplotlib colormap, default None
+        Colormap to use for line colors.
+    kwds: keywords
+        Options to pass to matplotlib plotting method
+
+    Returns
+    -------
+    ax: matplotlib axis object
+
+    Examples
+    --------
+    >>> from pandas import read_csv
+    >>> from pandas.tools.plotting import parallel_coordinates
+    >>> from matplotlib import pyplot as plt
+    >>> df = read_csv(\
+      'https://raw.github.com/pydata/pandas/master/pandas/tests/data/iris.csv')
+    >>> parallel_coordinates(\
+                           df, 'Name', color=('#556270', '#4ECDC4', '#C7F464'))
+    >>> plt.show()
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from pandas.compat import lrange
+    import pandas.core.common as com
+    from pandas.tools.plotting import _get_standard_colors
+
+    n = len(frame)
+    classes = frame[class_column].drop_duplicates()
+    class_col = frame[class_column]
+
+    if cols is None:
+        df = frame.drop(class_column, axis=1)
+    else:
+        df = frame[cols]
+
+    used_legends = set([])
+    ncols = len(df.columns)
+
+    # determine values to use for xticks
+    if use_columns is True:
+        if not np.all(np.isreal(list(df.columns))):
+            raise ValueError('Columns must be numeric to be used as xticks')
+        x = df.columns
+    elif xticks is not None:
+        if not np.all(np.isreal(xticks)):
+            raise ValueError('xticks specified must be numeric')
+        elif len(xticks) != ncols:
+            raise ValueError('Length of xticks must match number of columns')
+        x = xticks
+    else:
+        x = lrange(ncols)
+
+    if ax is None:
+        ax = plt.gca()
+
+    color_values = _get_standard_colors(num_colors=len_color,
+                                        colormap=colormap, color_type='random',
+                                        color=color)
+
+    colors = dict(zip(classes, [color_values[pos_color]]))
+
+    for i in range(n):
+        y = df.iloc[i].values
+        kls = class_col.iat[i]
+        label = com.pprint_thing(kls)
+        if label not in used_legends and is_use_legends:
+            used_legends.add(label)
+            ax.plot(x, y, color=colors[kls], label=label, **kwds)
+        else:
+            ax.plot(x, y, color=colors[kls], **kwds)
+
+    if no_seque:
+        for i in x:
+            ax.axvline(i, linewidth=0.25, color='black')
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(df.columns)
+    ax.set_xlim(x[0], x[-1])
+    ax.legend(loc='upper right')
+    ax.grid()
+    return ax
+    # ------------------------------------------------------------------
