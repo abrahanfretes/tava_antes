a569daf66fdd4ec138e741446aeccd1bac9e2f6c
Abrahan Fretes <abrahan.fretes@gmail.com>
2015-06-05 21:30:23 -0400

    Test Metricas
    Se agrega la función de auto agegación de test al árbol del proyecto
    al ser creado.
    
    Signed-off-by: Abrahan Fretes <abrahan.fretes@gmail.com>

--- py/una/pol/tava/presenter/metric/ptestmetric.py
+++ py/una/pol/tava/presenter/metric/ptestmetric.py
@@ -4,6 +4,9 @@ Created on 2/6/2015
 
 @author: abrahan
 '''
+from wx.lib.pubsub import Publisher as pub
+from py.una.pol.tava.presenter import topic as T
+
 from py.una.pol.tava.model.mmetric import MetricModel as mm
 from py.una.pol.tava.base.entity import TestMetric
 
@@ -11,6 +14,7 @@ from py.una.pol.tava.base.entity import TestMetric
 class TestMetricDialogPresenter():
     def __init__(self, iview,  project):
         self.iview = iview
+        self.project = project
         self.name = project.name
         self.id = project.id
         self.results = self.getResults(self.id)
@@ -33,4 +37,5 @@ class TestMetricDialogPresenter():
         test_metric.project_id = self.id
         test_metric.result_metric_id = self.results[index].id
         mm().addTestMetric(test_metric)
+        pub.sendMessage(T.PROJECT_UPDATE, self.project)
         self.iview.Close()
